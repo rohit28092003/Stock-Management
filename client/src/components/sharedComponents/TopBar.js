b12d0ea5bbe795d2230790356a8087b113fca3b5
keshav0100 <kbansal1_be22@thapar.edu>
2024-11-11 23:45:35 +0530

    initialize project structure with basic components and styles

---
+++ client/src/components/sharedComponents/TopBar.js
@@ -0,0 +1,29 @@
+import "./topbar.css";
+import { MdNotificationsNone, MdOutlineSettings } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+
+const TopBar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">StockIt</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <MdNotificationsNone size="30px" />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <MdOutlineSettings size="30px" />
+          </div>
+          <div className="topbarIconContainer">
+            <CgProfile size="30px" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
