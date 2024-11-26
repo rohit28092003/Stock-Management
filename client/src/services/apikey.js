1c99136177133f1b6c57ebcadafa9da109408d57
rohit28092003 <rohitdeepchandani@gmail.com>
2024-11-26 10:07:35 +0530

    yes

---
+++ client/src/services/apikey.js
@@ -0,0 +1,6 @@
+export const apikey = "OXPD71rA5DjHq4H8FTsrT0Yzk1FrEcke";
+
+export const apikeyPH = "OXPD71rA5DjHq4H8FTsrT0Yzk1FrEcke";
+// export const apikey = "eBUQ0AdUzScbnUsH61HsUcWk0kd7VGxz";
+// export const apikeyPH = "eBUQ0AdUzScbnUsH61HsUcWk0kd7VGxz";
+// eBUQ0AdUzScbnUsH61HsUcWk0kd7VGxz
